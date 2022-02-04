ba210c4b4a09fc110bc56b47f6f401c224379715
BryanGarcia97 <98426127+BryanGarcia97@users.noreply.github.com>
2022-02-03 19:12:31 -0600

    Update PrimerRepoJava.java

--- src/primerrepojava/PrimerRepoJava.java
+++ src/primerrepojava/PrimerRepoJava.java
@@ -15,7 +15,7 @@ public class PrimerRepoJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("HOLIS");
     }
     
 }
